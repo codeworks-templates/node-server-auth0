d5e774da0bf23d08a2e214a9201be40863e26414
Tim Dohmen <52214801+TimDohmen@users.noreply.github.com>
2020-09-10 12:18:42 -0600

    takes out edit profile

--- server/controllers/ProfilesController.js
+++ server/controllers/ProfilesController.js
@@ -9,20 +9,11 @@ export class ProfilesController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get("", this.getUserProfile)
-      .put("/:id", this.edit);
   }
   async getUserProfile(req, res, next) {
     try {
       let profile = await profilesService.getProfile(req.userInfo);
       res.send(profile);
-    } catch (error) {
-      next(error);
-    }
-  }
-  async edit(req, res, next) {
-    try {
-      req.body.creatorId = req.user.sub;
-      res.send(req.body);
     } catch (error) {
       next(error);
     }
