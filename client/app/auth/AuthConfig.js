593a8f82f9d299519e603d9da563d3718bc4affc
Jake Overall <joverall22@gmail.com>
2020-09-10 18:58:05 -0600

    update template

--- client/app/auth/AuthConfig.js
+++
@@ -1,5 +0,0 @@
-// TODO Add Auth0 Config
-export const domain = "";
-export const clientId = "";
-export const audience = "";
-
--- client/app/main.js
+++ client/app/main.js
@@ -1,8 +1,9 @@
-import { AuthController } from "./auth/AuthController.js";
-import { resource } from "./resource.js";
+import { AuthController } from "./Controllers/AuthController.js";
+import ValuesController from "./Controllers/ValuesController.js";
 
 class App {
   authController = new AuthController();
+  valuesController = new ValuesController();
 }
 
 window["app"] = new App();
