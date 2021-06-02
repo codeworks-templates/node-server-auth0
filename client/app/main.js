169cd2b412e4ca448699fe3ce53534cddb4288e8
Jake Overall <joverall22@gmail.com>
2021-06-02 00:18:13 -0600

    eslint formattings

--- client/app/main.js
+++ client/app/main.js
@@ -1,6 +1,6 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { SocketTestController } from './Controllers/SocketTestController.js'
-import ValuesController from './Controllers/ValuesController.js'
+import { ValuesController } from './Controllers/ValuesController.js'
 
 class App {
   authController = new AuthController();
