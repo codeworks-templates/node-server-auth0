06e95f72ffaba9397ba933cc0924d4b476c8715e
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2022-05-19 14:27:37 -0600

    Update DbContext.js

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose'
-import { AccountSchema, ProfileSchema } from '../models/Account'
+import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
 }
 
 export const dbContext = new DbContext()
