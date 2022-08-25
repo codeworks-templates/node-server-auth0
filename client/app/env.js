cfeb828864dad5d86e5224e1c39a65156851a44b
Jake Overall <joverall22@gmail.com>
2022-08-24 22:59:16 -0600

    removes default auth keys

--- client/app/env.js
+++ client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'classroomdemos.us.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
-export const clientId = '1RwYuMZc4P4BwvgujiBRqEybkumg1hww'
+export const domain = ''
+export const audience = ''
+export const clientId = ''
