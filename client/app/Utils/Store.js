a8b7d9e20336cce20a37645a70a3334e6728c77f
Jake Overall <joverall22@gmail.com>
2022-10-14 10:49:53 -0600

    Update Store.js

--- client/app/Utils/Store.js
+++ client/app/Utils/Store.js
@@ -1,12 +1,18 @@
 import { Pop } from "./Pop.js"
 
+const APP_NAME = "{{name}}"
+
 export function saveState(key, value) {
   try {
+    const keyName = `${APP_NAME}_${key}`
     let data = value
     if (typeof value != 'string') {
       data = JSON.stringify(data)
     }
-    window.localStorage.setItem(key, data)
+    window.localStorage.setItem(keyName, data)
+    if (typeof value == 'undefined' || value == null) {
+      window.localStorage.removeItem(keyName)
+    }
   } catch (error) {
     console.error('[SAVING_STATE]', { key, value })
     Pop.error(error)
@@ -15,9 +21,16 @@ export function saveState(key, value) {
 
 export function loadState(key, instanceType) {
   try {
-    let data = JSON.parse(window.localStorage.getItem(key) || '[]')
+    const keyName = `${APP_NAME}_${key}`
+    const keyType = Array.isArray(instanceType) ? '[]' : '{}'
+    instanceType = Array.isArray(instanceType) ? instanceType[0] : instanceType
+    let data = JSON.parse(window.localStorage.getItem(keyName) || keyType)
+    if (keyType == '{}' && !Object.keys(data).length) { return null }
     if (Array.isArray(data) && instanceType) {
       return data.map(i => new instanceType(i))
+    }
+    if (instanceType && data) {
+      return new instanceType(data)
     }
     return data
   } catch (error) {
