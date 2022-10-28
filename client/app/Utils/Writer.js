f9e8053108ad36ac773539bb62eaccfc30eaa968
Jake Overall <joverall22@gmail.com>
2022-10-28 11:27:48 -0600

    Update Writer.js

--- client/app/Utils/Writer.js
+++ client/app/Utils/Writer.js
@@ -1,13 +1,12 @@
 import { Pop } from "./Pop.js"
 
-const elems = {}
-
 function getElem(id) {
   try {
-    elems[id] = elems[id] || document.getElementById(id)
-    if (!elems[id]) {
-      throw new Error(`Invalid element Id ${id}`)
+    const elem = document.getElementById(id)
+    if (!elem) {
+      throw new Error('Invalid element Id ' + id)
     }
+    return elem
   } catch (error) {
     console.error('[ATTEMPTING_TO_SET_HTML]', id)
     Pop.error(error)
@@ -15,11 +14,9 @@ function getElem(id) {
 }
 
 export function setHTML(id, html) {
-  getElem(id)
-  elems[id].innerHTML = html
+  getElem(id).innerHTML = html
 }
 
 export function setText(id, text) {
-  getElem(id)
-  elems[id].innerText = text
+  getElem(id).innerText = text
 }
