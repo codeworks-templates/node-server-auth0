cd6f4b361a3f87cdf3351ed2c6284d4a20224a73
Jake Overall <joverall22@gmail.com>
2023-05-22 01:18:32 -0600

    removes default client

--- client/app/Services/AxiosService.js
+++
@@ -1,9 +0,0 @@
-import { baseURL } from '../env.js'
-
-// @ts-ignore
-// eslint-disable-next-line no-undef
-export const server = axios.create({
-  baseURL: baseURL,
-  timeout: 8000,
-  withCredentials: true
-})
