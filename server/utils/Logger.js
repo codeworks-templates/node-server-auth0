bf5136d694fcf7b2937cbea872ac93b1bd2e21f4
Jake Overall <joverall22@gmail.com>
2023-09-01 15:02:39 -0600

    Update Logger.js

--- server/utils/Logger.js
+++ server/utils/Logger.js
@@ -1,33 +1,45 @@
-function log(type, content) {
-  if (process.env.NODE_ENV === 'dev') {
-    // eslint-disable-next-line no-console
-    console[type](`[${type}] :: ${new Date().toLocaleTimeString()} :: `, ...content)
-  } else {
-    switch (type) {
-      case 'log':
-      case 'assert':
-        return
-    }
-    // TODO SEND LOGS TO EXTERNAL SERVICE
-    // eslint-disable-next-line no-console
-    console[type](`[${type}] :: ${new Date().toLocaleTimeString()} :: `, ...content)
+export class Logger {
+  constructor(tag = '') {
+    this.tag = tag
   }
-}
 
-export const logger = {
+  get prefix() {
+    return `${new Date().toLocaleTimeString('en-US')}${this.tag ? '::[' + this.tag + ']' : ''}`
+  }
+
+  _print(method, args) {
+    if (process.env.NODE_ENV === 'dev') {
+      // eslint-disable-next-line no-console
+      console[method](this.prefix, ...args)
+    } else {
+      switch (method) {
+        case 'log':
+        case 'debug':
+          return
+      }
+      // eslint-disable-next-line no-console
+      console[method](this.prefix, ...args)
+    }
+  }
+
   log() {
-    log('log', arguments)
-  },
-  error() {
-    log('error', arguments)
-  },
+    this._print('log', arguments)
+  }
+
+  debug() {
+    this._print('trace', arguments)
+  }
+
+  info() {
+    this._print('info', arguments)
+  }
+
   warn() {
-    log('warn', arguments)
-  },
-  assert() {
-    log('assert', arguments)
-  },
-  trace() {
-    log('trace', arguments)
+    this._print('warn', arguments)
+  }
+
+  error() {
+    this._print('error', arguments)
   }
 }
+export const logger = new Logger()
